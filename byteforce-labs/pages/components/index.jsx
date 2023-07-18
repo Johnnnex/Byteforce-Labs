e096af41b1c25f9460f40b0d434c02be07309277
Johnnnex <onyeiborjohn05@gmail.com>
2023-07-18 18:25:18 +0100

    our works

--- byteforce-labs/pages/components/index.jsx
+++ byteforce-labs/pages/components/index.jsx
@@ -28,6 +28,42 @@ const Homepage = () => {
                 </div>
             </section>
             <AboutUs />
+            <section className="mt-[8rem] w-[90%] mx-auto">
+                <h2 className=" font-poppins font-[700] text-[3rem]">Our Works</h2>
+                <p className=" font-robotoMono text-[1.3rem] mb-[2rem]">Use and reuse tons of components too</p>
+                <div className="flex mb-[3rem]">
+                    <div className="flex">
+                        <Image className='rounded-[8rem] h-full border-[1rem] border-white' src ="/Images/box.svg" alt='box-img' width={100} height={1} />
+                        <p className="w-[70%] font-robotoMono">
+                            <strong className=" font-poppins font-[700]">Vyperium</strong><br />
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, obcaecati? Facere impedit cumque delectus harum totam ab facilis voluptatum reiciendis, eos labore ex necessitatibus accusantium earum repellendus alias iste consectetur.
+                        </p>
+                    </div>
+                    <div className="flex">
+                        <Image className='rounded-[8rem] h-full border-[1rem] border-white' src ="/Images/box.svg" alt='box-img' width={100} height={1} /> 
+                        <p className="w-[70%] font-robotoMono">
+                            <strong className=" font-poppins font-[700]">Vyperium</strong><br />
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, obcaecati? Facere impedit cumque delectus harum totam ab facilis voluptatum reiciendis, eos labore ex necessitatibus accusantium earum repellendus alias iste consectetur.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div className="flex">
+                        <Image className='rounded-[8rem] h-full border-[1rem] border-white' src ="/Images/box.svg" alt='box-img' width={100} height={1} />
+                        <p className="w-[70%] font-robotoMono">
+                            <strong className=" font-poppins font-[700]">Vyperium</strong><br />
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, obcaecati? Facere impedit cumque delectus harum totam ab facilis voluptatum reiciendis, eos labore ex necessitatibus accusantium earum repellendus alias iste consectetur.
+                        </p>
+                    </div>
+                    <div className="flex">
+                        <Image className='rounded-[8rem] h-full border-[1rem] border-white' src ="/Images/box.svg" alt='box-img' width={100} height={1} />
+                        <p className="w-[70%] font-robotoMono">
+                            <strong className=" font-poppins font-[700]">Vyperium</strong><br />
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, obcaecati? Facere impedit cumque delectus harum totam ab facilis voluptatum reiciendis, eos labore ex necessitatibus accusantium earum repellendus alias iste consectetur.
+                        </p>
+                    </div>
+                </div>
+            </section>
         </main>
     </>
   )
