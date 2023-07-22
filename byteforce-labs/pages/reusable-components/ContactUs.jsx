787b17dcb952f3b9763079703f7dd7f4e372624c
Johnnnex <onyeiborjohn05@gmail.com>
2023-07-22 22:05:29 +0100

    contact us

--- byteforce-labs/pages/reusable-components/ContactUs.jsx
+++ byteforce-labs/pages/reusable-components/ContactUs.jsx
@@ -4,41 +4,65 @@ import Image from 'next/image'
 const ContactUs = () => {
   return (
     <>
-        <section className="">
-            <h3 className="">Contact Us</h3>
-            <p className="">
-                <b className="">Don't hesitate</b> <br />
+        <section className="mt-[8rem] w-[90%] mx-auto">
+            <h3 className="font-poppins font-[700] text-[1rem] bg-slate-300 w-fit px-3 rounded-md text-red-600">Contact Us</h3>
+            <p className=" font-robotoMono italic text-[1.3rem]">
+                <b className="font-poppins not-italic font-[700] text-[3rem]">Don't hesitate</b> <br />
                 to talk with us
             </p>
-            <p className=""> Relax, we're ready to support you 24/7</p>
-            <div className="flex">
-                <div className="">
-                    <div className="">
-                        <div className="bg-black">
-                            <Image src="/Images/subscribe.svg" alt='contact-icon' height={1} width={20} />
+            <p className="mt-[4px] font-robotoMono"> Relax, we're ready to support you 24/7</p>
+            <div className="flex justify-between mt-[4rem] items-center">
+                <div className=" space-y-[2rem] w-[40%]">
+                    <div className="flex items-center gap-4">
+                        <div className="flex items-center justify-center bxshadow bg-slate-400 p-4 rounded-[50%]">
+                            <Image src="/Images/mail.svg" alt='contact-icon' height={1} width={30} />
                         </div>
                         <div className="">
-                            <p>
-                                <small></small> <br />
-                                
-
+                            <p className="font-extrabold font-robotoMono">
+                                <small className="font-poppins text-[15px] font-thin">Email</small> <br />
+                                contact@byteforcelabs.com
                             </p>
                         </div>
                     </div>
-                    <div className="">
-                        <div className="bg-black">
-                            <Image src="/Images/subscribe.svg" alt='contact-icon' height={1} width={20} />
+                    <div className="flex items-center gap-4">
+                        <div className="flex items-center justify-center bxshadow bg-slate-400 p-4 rounded-[50%]">
+                            <Image src="/Images/phone.svg" alt='contact-icon' height={1} width={30} />
                         </div>
+                        
                         <div className="">
-                            <p>
-                                <small></small> <br />
-                                
-
+                            <p className="font-extrabold font-robotoMono">
+                                <small className="font-poppins text-[15px] font-thin">Phone</small> <br />
+                                +234 817 735 1189
                             </p>
                         </div>
                     </div>
                 </div>
-                <div className=""></div>
+                <form className="w-[50%] space-y-5">
+                    <h4 className="font-poppins font-extrabold text-[1.4rem]">Drop us a line</h4>
+                    <div className="flex justify-between">
+                        <div className="w-[49%]">
+                            <label className="font-poppins font-bold" htmlFor="name">
+                                Full Name
+                            </label> <br />
+                            <input placeholder="John Doe" type="text" id="name" className="w-full px-[1rem] py-2 font-robotoMono border-[#808080] outline-none border-[1px] rounded-md" />
+                        </div>
+                        <div className="w-[49%]">
+                            <label className="font-poppins font-bold" htmlFor="email">
+                                Email
+                            </label> <br />
+                            <input placeholder="Type your email" type="email" id="email" className="w-full px-[1rem] py-2 font-robotoMono border-[#808080] outline-none border-[1px] rounded-md" />
+                        </div>
+                    </div>
+                    <div className="">
+                        <label className="font-poppins font-bold" htmlFor="message">
+                            Message
+                        </label> <br />
+                        <input placeholder="Leave us a message" type="text" id="message" className="w-full px-[1rem] py-2 font-robotoMono border-[#808080] outline-none border-[1px] rounded-md"/>
+                    </div>
+                    <div className="">
+                        <button className="bg-red-400 px-4 py-2 rounded-[4px] font-poppins">Submit</button>
+                    </div>
+                </form>
             </div>
         </section>
     </>
